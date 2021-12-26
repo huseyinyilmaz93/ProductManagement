f103acad9256411c1e33f39fe2cf38177aab6003
huseyinyilmaz93 <huseyin.yilmaz093@gmail.com>
2021-12-26 20:03:38 +0300

    Docker implementations

--- PM.Api/Program.cs
+++ PM.Api/Program.cs
@@ -19,7 +19,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c => c.IncludeXmlComments(string.Format(@"{0}\PM.Api.xml", System.AppDomain.CurrentDomain.BaseDirectory)));
+
+builder.Services.AddSwaggerGen(c 
+    => c.IncludeXmlComments(string.Format(@"{0}PM.Api.xml", System.AppDomain.CurrentDomain.BaseDirectory)));
 
 builder.Services.AddSingleton<ICategoryService, CategoryService>();
 builder.Services.AddSingleton<IProductService, ProductService>();
@@ -46,11 +48,10 @@ var app = builder.Build();
 IoCHelper.SetServiceProvider(app.Services);
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
